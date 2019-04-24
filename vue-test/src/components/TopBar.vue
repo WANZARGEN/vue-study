9f0d2c93f0d2effb488308aaaab1e5718bd2e69b
wanzargen <wanjin911003@gmail.com>
2019-04-24 19:41:31 +0900

    adding templates

---
+++ vue-test/src/components/TopBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    })
+  }
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/common';
+
+.top-bar {
+    height: $top-bar-h;
+    width: 100vw;
+    color: white;
+    background-color: #4c5667;
+
+    .link {
+        color: white;
+        font-weight: 800;
+    }
+}
+</style>
