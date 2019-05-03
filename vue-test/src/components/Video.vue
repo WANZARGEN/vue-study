287c56d9909768e6ea2cf2f8c707fe1fc50b020b
wanzargen <wanjin911003@gmail.com>
2019-05-03 11:26:42 +0900

    Update Video.vue

--- vue-test/src/components/Video.vue
+++ vue-test/src/components/Video.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <!-- <iframe id="ytplayer" type="text/html" width="640" height="360"
+        <iframe id="ytplayer" type="text/html" width="640" height="360"
         src="http://www.youtube.com/embed?listType=playlist&list=PLC77007E23FF423C6"
-        frameborder="0"/> -->
+        frameborder="0"/> 
         <button v-bind:click="getWord">이회사인터넷거지같애-_-</button>
     </div>
 </template>
